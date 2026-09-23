8acc2b084aa02a597451c725a40aea8a15ea0af5
Ben Damerow <b_damerow@taures.de>
2026-09-23 10:26:46 +0200

    [boyscout] feat: no black page anymore while loading

--- lib/features/home/home_page.dart
+++ lib/features/home/home_page.dart
@@ -16,56 +16,51 @@ class HomePage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return switch (ref.watch(HomeCubit.provider)) {
-      HomeStateSuccess(:final projects) => _getContent(projects, ref),
-      _ => SizedBox.shrink(),
-    };
-  }
-
-  Widget _getContent(List<ProjectModel> projects, WidgetRef ref) {
     return BasicLayoutWrapper(
       showTitleBar: false,
       title: null,
       refreshAction: ref.read(HomeCubit.provider.bloc).loadProjects,
-      child: Column(
-        children: [
-          Row(
-            children: [
-              TTextLarge(
-                'Deine Projekte',
-                bold: true,
-              ),
-            ],
-          ),
-          SizedBox(height: TSpacings.p0),
-          Row(
-            children: [
-              Container(
-                width: 250,
-                child: TTextSmall(
-                  'Hier findest du eine Übersicht über all deine VisionFlow Projekte',
-                ),
-              ),
-            ],
-          ),
-          SizedBox(height: TSpacings.p1),
-          ListView(
-            padding: EdgeInsetsGeometry.all(0),
-            shrinkWrap: true,
-            physics: NeverScrollableScrollPhysics(),
-            children: [...projects.map((project) => HomeProjectWidget(project))],
-          ),
-          Padding(
-            padding: const EdgeInsets.only(bottom: TSpacings.p0),
-            child: IconButton(
-              // ToDo: add new Project
-              onPressed: () => print('pressed on add new Project'),
-              icon: TIcon(Icons.add_circle_outline, color: TColors.textPrimary),
-              iconSize: TIconSizes.normal,
+      child: switch (ref.watch(HomeCubit.provider)) {
+        HomeStateSuccess(:final projects) => _getContent(projects, ref),
+        _ => SizedBox.shrink(),
+      },
+    );
+  }
+
+  Widget _getContent(List<ProjectModel> projects, WidgetRef ref) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            TTextLarge(
+              'Deine Projekte',
+              bold: true,
             ),
+          ],
+        ),
+        SizedBox(height: TSpacings.p0),
+        Row(
+          children: [
+            Container(
+              width: 250,
+              child: TTextSmall(
+                'Hier findest du eine Übersicht über all deine VisionFlow Projekte',
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: TSpacings.p1),
+        ...projects.map((project) => HomeProjectWidget(project)),
+        Padding(
+          padding: const EdgeInsets.only(bottom: TSpacings.p0),
+          child: IconButton(
+            // ToDo: add new Project
+            onPressed: () => print('pressed on add new Project'),
+            icon: TIcon(Icons.add_circle_outline, color: TColors.textPrimary),
+            iconSize: TIconSizes.normal,
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
